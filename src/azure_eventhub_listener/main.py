36edac74467c2d2c3a524facb3d42e6a489cdd2c
sameervchaudhari <153898302+sameervchaudhari@users.noreply.github.com>
2023-12-21 15:17:23 +0530

    Create main.py

---
+++ src/azure_eventhub_listener/main.py
@@ -0,0 +1,16 @@
+import asyncio
+
+async def on_event(partition_context, event):
+    print("Received event:", event.body_as_str())
+    # Process the event here
+
+async def start_listening():
+    print("package is running .....")
+    # Async operations go here, make sure to use 'await' for each async call
+
+def main():
+    asyncio.run(start_listening())
+
+if __name__ == "__main__":
+    main()
+
